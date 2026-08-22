59bbec59873880a19572bcd81349dca5beb541ef
Yamini1025 <yamini.karthik06@gmail.com>
2026-08-21 19:11:59 -0700

    delete functionality and itinerary button

--- frontend/src/TripForm.jsx
+++ frontend/src/TripForm.jsx
@@ -85,7 +85,7 @@ function TripForm({ onSubmit, onClose }) {
             <input
               name="adults"
               type="number"
-              min="1"
+              min="0"
               value={form.adults}
               onChange={handleChange}
               placeholder="Number of adults"
@@ -102,6 +102,7 @@ function TripForm({ onSubmit, onClose }) {
               value={form.children}
               onChange={handleChange}
               placeholder="Number of children"
+              required
             />
           </label>
           <label>
